c3150a637e70a319a1d76776b7d80ef8a27fd7e7
Yashada <nalawadeyashada@gmail.com>
2024-01-12 17:23:18 +0530

    - changes 2

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,6 +4,15 @@ import image1 from "../Assets/galleryImages/image1.png"
 import event_1 from "../Assets/galleryImages/event_1.png"
 import event_8 from "../Assets/galleryImages/event_8.png"
 import event_6 from "../Assets/galleryImages/event_6.png"
+import event_2 from "../Assets/galleryImages/event_2.jpg"
+import image_9 from "../Assets/galleryImages/image_9.jpg"
+import image_10 from "../Assets/galleryImages/image_10.jpg"
+import image_11 from "../Assets/galleryImages/image_11.jpg"
+import image_12 from "../Assets/galleryImages/image_12.jpg"
+import image_13 from "../Assets/galleryImages/image_13.jpg"
+import image_14 from "../Assets/galleryImages/image_14.jpg"
+import image_15 from "../Assets/galleryImages/image_15.jpg"
+import image_16 from "../Assets/galleryImages/image_16.jpg"
 
 export default function gallery() {
 
@@ -11,9 +20,16 @@ export default function gallery() {
         image1,
         event_1,
         event_8,
-        event_6,
-        image1,
-        event_1,
+        event_6,       
+        event_2,
+        image_9,
+        image_10,
+        image_11,
+        image_12,
+        image_13,
+        image_14,
+        image_15,
+        image_16,
 
        
     ]
