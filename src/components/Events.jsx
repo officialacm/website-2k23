1cf4720776c32ae7e0500bdee7e2e3f07d58c0ab
1447bits <untilinfinity00@gmail.com>
2024-01-12 20:35:06 +0530

    design fixes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import "../styles/events.css"
 import codeQuestImg from "../Assets/eventPosters/codeQuest.png"
 import event_1 from "../Assets/eventPosters/event_1.png"
-import event_2 from "../Assets/eventPosters/event_2.png"
 import event_8 from "../Assets/eventPosters/event_8.png"
 import event_6 from "../Assets/eventPosters/event_6.png"
 import event_7 from "../Assets/eventPosters/event_7.png"
@@ -13,9 +12,7 @@ const eventData = [
     time: "9:15 AM",
     image: event_1,
     eventDesc: [
-      "Event Coordinators",
-      "Event Coordinators",
-      "Event Coordinators"
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in."
     ],
     link: "#"
   },
@@ -24,20 +21,16 @@ const eventData = [
     time: "9:15 AM",
     image: event_6,
     eventDesc: [
-      "Event Coordinators",
-      "Event Coordinators",
-      "Event Coordinators"
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in."
     ],
     link: "#"
   },
   {
     date: "january 15",
     time: "9:15 AM",
-    image: event_1,
+    image: codeQuestImg,
     eventDesc: [
-      "Event Coordinators",
-      "Event Coordinators",
-      "Event Coordinators"
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in."
     ],
     link: "#"
   },
@@ -46,9 +39,7 @@ const eventData = [
     time: "9:15 AM",
     image: event_8,
     eventDesc: [
-      "Event Coordinators",
-      "Event Coordinators",
-      "Event Coordinators"
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in."
     ],
     link: "#"
   },
@@ -57,46 +48,58 @@ const eventData = [
     time: "9:15 AM",
     image: event_7,
     eventDesc: [
-      "Event Coordinators",
-      "Event Coordinators",
-      "Event Coordinators"
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, in."
     ],
     link: "#"
   },
 ]
 
 export function EventCardStack() {
+
   let allcards = []
   let thisevent = eventData[1]
+
   for (let i = 0; i < eventData.length; i++) {
+
     thisevent = eventData[i]
+
+    // push cards for each event in allcards array
     allcards.push(<div className='EventCardStackContainer'>
       <div className='EventcardStackHeader'>
+
         <span style={{
           marginInline: "auto",
         }}>
           <h2>{thisevent.date}</h2>
           <p>{thisevent.time}</p>
         </span>
+
       </div>
+
       <div style={{
         flexDirection: i % 2 === 0 ? "column" : "column-reverse",
       }} className='EventCardStack'>
+
         <div className='EventCardStackImageCard'>
           <img src={thisevent.image} alt="" />
         </div>
+
         <div className='EventCardStackDetailsCard'>
-          <p className='date'>{thisevent.date}</p>
+          <p className='date'>{thisevent.eventDesc}</p>
+
           <span>
             {thisevent.eventDesc.forEach(e => {
               return <p>{e}</p>
             })}
           </span>
-          <p className='formLink'>
+
+          <span className='formLink'>
             <p>Free</p>
-            <a href={thisevent.link}>Form Link {"->"}</a>
-          </p>
+            <a href={thisevent.link}>Form Link 🔗</a>
+          </span>
+
         </div>
+
       </div>
     </div>)
   }
